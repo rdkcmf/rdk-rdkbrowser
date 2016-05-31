10cdcfe7f36efc9518081e6b57c28c6326db6290
Jim Lawton <jim.lawton@accenture.com>
2016-05-31 06:11:25 +0100

    RDKCMF-3658  Update from Comcast source (master).
     * Internal server error in Emulator with device.html
    
    Change-Id: I7a6d93427882e99bd5b6f0e4c5f16841ef6ff39c
    Source: https://gerrit.teamccp.com/rdk/components/generic/rdkbrowser/soc/intel-x86-pc/rdkemulator
    SHA1: da1c2db1d0240191e6e10ff20cf2b5bf8f1c1f28

--- html/getip.sh
+++ html/getip.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-while true
+# do clear IP address html file if any
+sed -i -e  "s/IP Address\:.*$/IP Address\:  /g" /opt/www/deviceinfo.html
+
+loop=1
+while [ $loop -eq 1 ]
 do
         ipaddr=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 
 
+        if [ "$ipaddr" ]; then
+         loop=0
         # do inplace replace of html file
         sed -i -e  "s/IP Address\:.*$/IP Address\: ${ipaddr}/g" /opt/www/deviceinfo.html
+        fi
+
 done
